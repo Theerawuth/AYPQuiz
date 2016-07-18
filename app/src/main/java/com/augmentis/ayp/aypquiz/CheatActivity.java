2768861c5a66ec1b9a9a03644273ecfb4643223e
theerawuth <theerawuth.p@augmentis.biz>
2016-07-18 10:48:33 +0700

    QuizAndroid- Finish Quiz

--- app/src/main/java/com/augmentis/ayp/aypquiz/CheatActivity.java
+++ app/src/main/java/com/augmentis/ayp/aypquiz/CheatActivity.java
@@ -1,10 +1,17 @@
 package com.augmentis.ayp.aypquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.animation.AnimatorListenerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ViewPropertyAnimatorCompatSet;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -45,19 +52,40 @@ public class CheatActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //
-                if(answer){
+                if (answer) {
                     answerText.setText(R.string.answer_is_true);
-                }else{
+                } else {
                     answerText.setText(R.string.answer_is_false);
                 }
 
 
                 isCheated = true;
                 returnResult();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    //Add animator
+                    int cx = answerText.getWidth() / 2;
+                    int cy = answerText.getHeight() / 2;
 
+                    float radius = answerText.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(answerText, cx, cy, radius, 0);
+
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+
+                            answerText.setVisibility(View.VISIBLE);
+                            confirmButton.setVisibility(View.INVISIBLE);
+                        }
+
+                    });
+                    anim.start();
+                } else {
+                    answerText.setVisibility(View.VISIBLE);
+                    confirmButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
-
 
     }
 
